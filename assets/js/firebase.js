43b5e465eaef75fea013227b4daef25ff1a245e8
Jacob Tate <jacob87608@gmail.com>
2020-02-10 19:24:52 -0600

    removed the copy-paste errors

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -10,12 +10,12 @@ var firebaseConfig = {
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
-  ​
+
   //Variable created to refernce database
   var auth = firebase.auth();
   var db = firebase.firestore();
-  ​
-  ​
+
+
   //Authentication Status Listener to determine if a user is signed in or not
   //--------------------------------------
   auth.onAuthStateChanged(user => {
@@ -26,18 +26,18 @@ var firebaseConfig = {
       $("#user-para").show();
       $("#login-div").hide();
       $("#user-signup").hide();
-  ​
+
       //Checks for currentUser and displays user email address
       var user = auth.currentUser;
-  ​
+
       if(user != null){
-  ​
+
         var email_id = user.email;
-  ​
+
         $("#user-para").html("Welcome " + email_id);
-  ​
+
       }
-  ​
+
     } else {
       // No user is signed in.
       $("#user-div").hide();
@@ -46,28 +46,28 @@ var firebaseConfig = {
       $("#login-div").hide();
     }
   });
-  ​
+
   //On Sign Up button click
   $("#sign-up").on("click", function(){
     $("#user-signup").get(0).reset();
-    $("#user-signup").toggle()
-      
+    $("#user-signup").toggle();
+
   });
-  ​
+
   //On Login button click
   $("#login").on("click", function(){
-    
+
     $("#login-div").toggle();
   })
-  ​
+
   //---------------------------------------
   //On Sign Up submit button click (Sign up Div)
   $("#signup-submit").on("click", function(){
     event.preventDefault();
-  ​
+
     var signUpEmail = $("#inputEmail").val().trim();
     var signUpPass = $("#inputPassword").val().trim();
-  ​
+
     db.collection("users").add({
         name: signUpEmail,
         password:  signUpPass
@@ -78,71 +78,68 @@ var firebaseConfig = {
     .catch(function(error){
         console.error("Error adding document: ", error);
     });
-  ​
+
     //console.log(signUpEmail + signUpPass);
-  ​
+
     auth.createUserWithEmailAndPassword(signUpEmail, signUpPass).then(cred => {
       console.log(cred);
       $("#user-signup")[0].reset();
-  ​
+
     });
-    
+
   })
-  ​
-  ​
-  ​
+
+
+
   //----------------------------------------
   //On Login button click (Login Div)
   $("#login-submit").on("click", function(){
     event.preventDefault();
-  ​
+
     var userEmail = $("#email-field").val().trim();
     var userPassword = $("#password-field").val().trim();
-  ​
+
     auth.signInWithEmailAndPassword(userEmail, userPassword).then(cred =>{
         console.log(cred.user)
         $("#login-div")[0].reset();
     })
   })
-  ​
+
   //------------------------------------------------
   //On Logout button click (Logout Div)
   $("#logout-submit").on("click", function(){
     auth.signOut();
     $("#drinkList").empty();
   })
-  ​
-  ​
+
+
   //---------------------------------------------------
   //Drink History
-  ​
+
   $("#drink-history").on("click",(cred => {
-  ​
+
     var userDrink = 'Whiskey Sour';
     //var userDrink = ("#drinkSelector").val();
-  ​
+
     console.log(userDrink);
-  ​
+
     $("#drinkList").text(userDrink);
-  ​
+
     //Get the existing data
     var existing = localStorage.getItem('drink');
-  ​
+
     //if no existing data, create an array
     //otherwise, convert the localStorage string to an array
     existing = existing ? existing.split(',') : [];
-  ​
+
     //Add new data to localStorage Array
     existing.push(userDrink);
-  ​
+
     //Save back to localStorage
     localStorage.setItem('drink', existing.toString());
-  ​
-    
-  ​
+
+
    $("#drinkList").text(localStorage.getItem("drink"));
-  ​
-    
-  ​
+
+
   }))
-  
